7590075ede1eb28f1d13b6dc8b6314754f4eb67a
skc4365 <50658047+skc4365@users.noreply.github.com>
2025-01-15 22:00:09 +0900

    controller추가

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("/")
+	public String getMethodName() {
+		return "test";
+	}
+	
+
+}
